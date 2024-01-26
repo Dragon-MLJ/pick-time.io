50da3ec3fe7ea4a13aab18b5d15878092499e03b
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-26 11:39:46 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="https://cdn.jsdelivr.net/gh/Dragon-MLJ/Dragon-MLJ-Figure-bed/img/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700;800;900&family=Noto+Sans+TC:wght@400;500;700;900&display=swap" rel="stylesheet" />
