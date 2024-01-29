0639fbb7def642ca9cfd7e0935744abd005beebb
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 13:50:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/picktime' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/picktime' : '',
+  optimizeFonts: false,
   i18n
 };
 
