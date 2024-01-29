33fe2e5e0161796966d033101170a63a4afcf453
Dragon-MLJ <128272207+Dragon-MLJ@users.noreply.github.com>
2024-01-29 13:55:59 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NODE_ENV === 'production' ? 'https://pick.mlj-dragon.cn' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://pick.mlj-dragon.cn' : '',
-  optimizeFonts: false,
   i18n
 };
 
